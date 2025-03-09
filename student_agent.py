039434203f0f179ae200dabdc27ba52869b62e59
Leoxu3 <b11902129@ntu.edu.tw>
2025-03-09 19:05:18 +0800

    q-learing based on RealTaxiEnv(stations)

--- student_agent.py
+++ student_agent.py
@@ -14,7 +14,12 @@ def get_action(obs):
     
     with open("q_table.pkl", "rb") as f:
         Q = pickle.load(f)
-    state = (obs[10], obs[11], obs[12], obs[13], obs[14], obs[15])
+    taxi_loc = (obs[0], obs[1])
+    stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])] 
+    if taxi_loc in stations:
+        state = (True, obs[10], obs[11], obs[12], obs[13], obs[14], obs[15])
+    else:
+        state = (False, obs[10], obs[11], obs[12], obs[13], obs[14], obs[15])
     if state not in Q:
         return random.randint(0, 5)
     return int(np.argmax(Q[state]))
