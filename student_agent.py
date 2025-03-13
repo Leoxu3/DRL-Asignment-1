2aeac5ed84c6f62a2f7bb0b0625647ffdc3a11f2
Leoxu3 <b11902129@ntu.edu.tw>
2025-03-13 19:45:54 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -8,13 +8,13 @@ def get_action(obs):
     obstacle_south = obs[11]
     obstacle_east = obs[12]
     obstacle_west = obs[13]
-    if obstacle_north != 1:
+    if obstacle_north == 0:
         action = 1
-    elif obstacle_east != 1:
+    elif obstacle_east == 0:
         action = 2
-    elif obstacle_south != 1:
+    elif obstacle_south == 0:
         action = 0
-    elif obstacle_west != 1:
+    elif obstacle_west == 0:
         action = 3
     else:
         action = random.randint(4,5)
