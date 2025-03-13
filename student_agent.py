0a58e5a5c39dd29332b68a665730d13fc1cc7635
Leoxu3 <b11902129@ntu.edu.tw>
2025-03-13 19:23:01 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -1,87 +1,19 @@
 # Remember to adjust your student ID in meta.xml
 import numpy as np
 import pickle
-import random
 
 def get_action(obs):
-    global passenger_picked_up
-    global q_table
-    global pre_action
-    # TODO: Train your own agent
-    # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
-    # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
-    #       Otherwise, even if your agent performs well in training, it may fail during testing.
-    def get_state(obs, passenger_picked_up, pre_action):
-        taxi_loc = (obs[0], obs[1])
-        stations = [(obs[2], obs[3]), (obs[4], obs[5]), (obs[6], obs[7]), (obs[8], obs[9])]
-        obstacle_north = obs[10]
-        obstacle_south = obs[11]
-        obstacle_east = obs[12]
-        obstacle_west = obs[13]
-        passenger_look = obs[14]
-        destination_look = obs[15]
-
-        station_north =False
-        station_south = False
-        station_east = False
-        station_west = False
-        station_middle = False
-        for station in stations:
-            if (taxi_loc[0] - 1, taxi_loc[1]) == station:
-                station_north = True
-            if (taxi_loc[0] + 1, taxi_loc[1]) == station:
-                station_south = True
-            if (taxi_loc[0], taxi_loc[1] + 1) == station:
-                station_east = True
-            if (taxi_loc[0], taxi_loc[1] - 1) == station:
-                station_west = True
-            if taxi_loc == station:
-                station_middle = True
-
-        return (obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look, passenger_picked_up, station_north, station_south, station_east, station_west, station_middle, pre_action)
-
-    def softmax(x):
-        exp_x = np.exp(x - np.max(x))
-        return exp_x / exp_x.sum()
-    
-    '''
-    state = get_state(obs, passenger_picked_up, pre_action)
-    if state not in q_table:
-        action = random.randint(0, 5)
-    else:
-        probs = softmax(q_table[state])
-        print(probs)
-        print(state)
-        action = np.random.choice(range(6), p=probs)
-
-    if action == 4 and state[4] == 1 and state[5]:
-        passenger_picked_up = True
-    if action == 5 and state[6] == 1 and state[5]:
-        passenger_picked_up = False
-
-    pre_action = action
-
-    return action'
-    '''
     obstacle_north = obs[10]
     obstacle_south = obs[11]
     obstacle_east = obs[12]
     obstacle_west = obs[13]
-    if obstacle_north == 0:
-        return 1
-    if obstacle_south == 0:
-        return 0
-    if obstacle_east == 0:
+    if obstacle_east != 1:
         return 2
-    if obstacle_west == 0:
+    if obstacle_west != 1:
         return 3
+    if obstacle_north != 1:
+        return 1
+    if obstacle_south != 1:
+        return 0
     else:
-        return None
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
-
-with open("q_table.pkl", "rb") as f:
-    q_table = pickle.load(f)
-passenger_picked_up = False
-pre_action = None
+        return 4
