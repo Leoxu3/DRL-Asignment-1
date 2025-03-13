709c2874b2739d9b36a41d8c1200deb5958a89f0
Leoxu3 <b11902129@ntu.edu.tw>
2025-03-13 19:34:27 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -1,5 +1,6 @@
 # Remember to adjust your student ID in meta.xml
 import numpy as np
+import random
 import pickle
 
 def get_action(obs):
@@ -7,13 +8,13 @@ def get_action(obs):
     obstacle_south = obs[11]
     obstacle_east = obs[12]
     obstacle_west = obs[13]
-    if obstacle_east != 1:
-        return 2
-    if obstacle_west != 1:
-        return 3
     if obstacle_north != 1:
         return 1
+    if obstacle_east != 1:
+        return 2
     if obstacle_south != 1:
         return 0
+    if obstacle_west != 1:
+        return 3
     else:
-        return 4
+        return random.randint(4,5)
